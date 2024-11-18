37864a777a3cfeb43d03dc5b2fe3e85c2f19b2d6
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-11-18 23:46:01 +0100

    wip - to remove

--- src/rmscene/__main__.py
+++ src/rmscene/__main__.py
@@ -2,8 +2,8 @@
 
 import sys
 import argparse
-from . import read_blocks
-
+from . import read_blocks, build_tree, SceneTree
+from rmscene import scene_items as si
 
 def parse_args(args):
     parser = argparse.ArgumentParser(prog="rmscene")
@@ -11,10 +11,24 @@ def parse_args(args):
     return parser.parse_args(args)
 
 
+def recursive(item: si.Group):
+    for child_id in item.children:
+        child = item.children[child_id]
+        if isinstance(child, si.Group):
+            recursive(child)
+        elif isinstance(child, si.Line):
+            pass
+
 def pprint_file(args) -> None:
     import pprint
 
     result = read_blocks(args.file)
+    tree = SceneTree()
+    build_tree(tree, result)
+
+
+    recursive(tree.root)
+
     for el in result:
         print()
         pprint.pprint(el)
